98cf720f2a2ea2cf44282b466d16a890b148615c
Jinming-Zhang <327429979@qq.com>
2019-03-03 16:38:24 -0500

    usermainpage update

--- js/PostingModal.js
+++ js/PostingModal.js
@@ -1,18 +1,21 @@
 'use strict';
-
 // get elements
 const postButton = document.querySelector('#newPostButtonDiv');
 const postModal = document.querySelector('#PostModal');
+// post form elements
+const postModalForm = document.querySelector('#PostModal-Form');
+const postImgDiv = document.querySelector('#PostModal-Picture');
+const postImageFileSelector = document.querySelector('#PostModal-ImgFileSelector');
 const submitPostButton = document.querySelector('#PostModal-SubmitPost');
+
 
 postButton.addEventListener('click', showModal);
 window.addEventListener('click', closeModal);
-submitPostButton.addEventListener('click', addPost);
-
-// testing posting object
-const newPosting = new Posting('../Image/samplePost1.jpg','Posting1','comment1');
+postImageFileSelector.addEventListener('change', parsePostImg);
+postModalForm.addEventListener('submit', parsePostImg);
 
 
+// post modal displays
 function showModal(e){
 	postModal.style.display = 'block';
 }
@@ -23,9 +26,34 @@ function closeModal(e){
 	}
 }
 
-function addPost(){
+
+// posting parsing and post
+// dragover files to upload img
+postImgDiv.addEventListener('click', function(e){
+	console.log('click');
+	postImageFileSelector.click();
+});
 
 
-	(new PostingManager(document.querySelector('#UserPostingsTable'), 4)).appendPostingDOM(newPosting);
+function parsePostImg(e){
+	e.preventDefault();
+
+	const newPostImgFile = postImageFileSelector.files[0];
+	// read the img url
+	const fileReader = new FileReader();
+
+	fileReader.onload = ()=>{
+		const newPosting = new Posting(fileReader.result,'Posting1','comment1');
+		addPost(newPosting);
+	}
+	fileReader.readAsDataURL(newPostImgFile);
+
+}
+
+
+function addPost(newPost){
+
+
+	(new PostingManager(document.querySelector('#UserPostingsTable'), 4)).appendPostingDOM(newPost);
 	postModal.style.display = 'none';
 }
